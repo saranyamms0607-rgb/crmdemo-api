a7c7ac8ab8b85ef1adaa2de1312fac4648e7319c
adithyanmms1120 <adithyanmms1120@gmail.com>
2025-12-30 17:04:00 +0530

    deployed in render

--- CRMProject/CRMProject/settings.py
+++ CRMProject/CRMProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q@$$9ze$l=m43w$)udl7v_vqc5a0pl3r8voi(&=%8t1ovw(gjj'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=False)
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    cast=lambda v: v.split(",")
+)
 
 
 
@@ -162,3 +166,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "saranyamms0607@gmail.com"
 EMAIL_HOST_PASSWORD = "vxgoprfyqmvndmsh"  # NOT gmail password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+
+
